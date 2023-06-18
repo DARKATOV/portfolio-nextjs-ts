2b44bdd73d7b907d8831b84d1f63e816db46055f
Florentino Carrera <florentinocarrera44@gmail.com>
2023-06-18 22:00:38 +0200

    Creando pantallas: - Acerca de. - Chat. - Generador de imagenes.

---
+++ pages/projects/chat.tsx
@@ -0,0 +1,9 @@
+import Title from '@/components/Title'
+
+export default function Chat() {
+  return (
+    <main className="container">
+      <Title text="Chat" href="/projects" />
+    </main>
+  )
+}
---
+++ pages/projects/images-generator.tsx
@@ -0,0 +1,9 @@
+import Title from '@/components/Title'
+
+export default function ImagesGenerator() {
+  return (
+    <main className="container">
+      <Title text="Generador de imágenes" href="/projects" />
+    </main>
+  )
+}
