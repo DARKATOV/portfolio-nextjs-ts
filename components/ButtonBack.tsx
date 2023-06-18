46603d27dec34ce8ff9e72e3b8c1375a12f6d846
Florentino Carrera <florentinocarrera44@gmail.com>
2023-06-18 21:58:35 +0200

    Creando componentes ButtonBack y Title.

---
+++ components/ButtonBack.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import Image from 'next/image';
+
+type Props = {
+  href: string
+}
+
+export default function ButtonBack({ href }: Props) {
+  const image = <Image
+    src="/icons/arrow-left.svg"
+    alt="Back"
+    width={20}
+    height={20}
+    priority
+  />
+
+  if (!href) {
+    return (
+      <button className="btn btn-light" onClick={() => history.back()}>
+        {image} Volver
+      </button>
+    )
+  }
+
+  return (
+    <Link href={href} className="btn btn-light">
+      {image} Volver
+    </Link>
+  )
+}
---
+++ components/Title.tsx
@@ -0,0 +1,19 @@
+import ButtonBack from '@/components/ButtonBack'
+
+type Props = {
+  text: string,
+  href: string,
+}
+
+export default function Title({ text, href }: Props) {
+  return (
+    <div className="row">
+      <div className="col-md-3">
+        <ButtonBack href={href} />
+      </div>
+      <div className="col-md-4 offset-md-2">
+        <h2>{text}</h2>
+      </div>
+    </div>
+  )
+}
