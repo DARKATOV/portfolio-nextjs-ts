f490ccf7d222f1cee67ef424e0b257dc9a752857
Florentino Carrera <florentinocarrera44@gmail.com>
2023-06-19 00:51:45 +0200

    Creando vista de producto.

---
+++ pages/api/products/[id].ts
@@ -0,0 +1,58 @@
+import { NextApiResponse, NextApiRequest } from 'next'
+import dbConnect from '@/lib/dbConnect'
+import ProductSchema from '@/models/Product'
+import type { Product, ResponseError } from '@/interfaces'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Product | ResponseError>
+) {
+  const {
+    query: { id },
+    method,
+  } = req
+
+  await dbConnect()
+
+  switch (method) {
+    case 'GET':
+      try {
+        const product = await ProductSchema.findById(id)
+        if (!product) {
+          return res.status(400).json({ message: 'El producto no existe.' })
+        }
+        res.status(200).json(product)
+      } catch (error) {
+        res.status(400).json({ message: 'Error al obtener producto.' })
+      }
+      break
+    case 'PUT':
+      try {
+        const product = await ProductSchema.findByIdAndUpdate(id, req.body, {
+          new: true,
+          runValidators: true,
+        })
+        if (!product) {
+          return res.status(400).json({ message: 'El producto no existe.' })
+        }
+        res.status(200).json(product)
+      } catch (error) {
+        res.status(400).json({ message: 'Error al modificar producto.' })
+      }
+      break
+    case 'DELETE':
+      try {
+        const deletedProduct = await ProductSchema.deleteOne({ _id: id })
+        if (!deletedProduct) {
+          return res.status(400).json({ message: 'El producto no existe.' })
+        }
+        res.status(200).json({ message: 'Producto eliminado con éxito.' })
+      } catch (error) {
+        res.status(400).json({ message: 'Error al eliminar producto.' })
+      }
+      break
+    default:
+      res.status(400).json({ message: 'Método no válido.' })
+      break
+  }
+}
---
+++ pages/projects/crud/[id]/index.tsx
@@ -0,0 +1,105 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { Product, ResponseError } from '@/interfaces'
+import Link from 'next/link'
+import useSWR from 'swr'
+import Title from '@/components/Title'
+import styles from '@/styles.module.css'
+
+const fetcher = async (url: string) => {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  if (res.status !== 200) {
+    throw new Error(data.message)
+  }
+  return data
+}
+
+export default function ProductPage() {
+  const router = useRouter()
+  const [message, setMessage] = useState('')
+  const { id } = router.query
+  const {
+    data: product,
+    error,
+    isLoading,
+  } = useSWR<Product, ResponseError>(id ? `/api/products/${id}` : null, fetcher)
+
+  const handleDelete = async () => {
+    try {
+      await fetch(`/api/products/${id}`, {
+        method: 'DELETE',
+      })
+      router.push('/projects/crud')
+    } catch (error) {
+      setMessage('Error al eliminar el producto.')
+    }
+  }
+
+  if (error) return (
+    <main className="container">
+      <Title text="Producto" href="/projects/crud" />
+
+      <h3>Hubo un error</h3>
+    </main>
+  )
+
+  if (isLoading) return (
+    <main className="container">
+      <Title text="Producto" href="/projects/crud" />
+
+      <div className={styles.loader}></div>
+    </main>
+  )
+
+  if (!product) return (
+    <main className="container">
+      <Title text="Producto" href="/projects/crud" />
+    </main>
+  )
+
+  return (
+    <main className="container">
+      <Title text="Producto" href="/projects/crud" />
+
+      <form>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">Nombre</label>
+          <input
+            id="name"
+            type="text"
+            className="form-control"
+            value={product.name}
+            disabled
+            readOnly
+            autoComplete={'off'}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="price" className="form-label">Precio</label>
+          <input
+            id="price"
+            type="number"
+            className="form-control"
+            value={product.price}
+            disabled
+            readOnly
+            autoComplete={'off'}
+          />
+        </div>
+
+        <Link
+          href={`/projects/crud/${product._id}/edit`}
+          className="btn btn-warning me-4">
+          Editar
+        </Link>
+        <button type="button" className="btn btn-danger" onClick={handleDelete}>
+          Eliminar
+        </button>
+      </form>
+      {message && <p>{message}</p>}
+    </main>
+  )
+}
