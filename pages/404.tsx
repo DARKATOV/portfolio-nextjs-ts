d0f081c0b656638fd2a68f6bc4d83bc873844a80
Florentino Carrera <florentinocarrera44@gmail.com>
2023-06-27 17:54:10 +0200

    Creando páginas de error.

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <main className="container">
+      <h2>404 - Página no encontrada</h2>
+    </main>
+  )
+}
---
+++ pages/500.tsx
@@ -0,0 +1,7 @@
+export default function Custom500() {
+  return (
+    <main className="container">
+      <h2>500 - Ocurrió un error en el servidor</h2>
+    </main>
+  )
+}
