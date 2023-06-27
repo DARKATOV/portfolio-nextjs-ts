7a94a3f7672ea4c3ce39854f6338ad3dfe19ca24
Florentino Carrera <florentinocarrera44@gmail.com>
2023-06-27 19:48:14 +0200

    Agregando contenido en Inicio y Acerca de.

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <main className="container">
-      <h2>Acerca de</h2>
+      <h2>Florentino Carrera</h2>
     </main>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
     <main className="container">
-      <h2>Inicio</h2>
+      <h2>Hola visitante</h2>
+
+      <p>Mi nombre es Florentino Carrera y este es un portafolio desarrollado
+        con la tecnología de Next.js.
+      </p>
+
+      <p>Visita la pantalla de proyectos para explorar más por esta web.</p>
     </main>
   )
 }
