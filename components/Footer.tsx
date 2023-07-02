4b880a48f899891b1536bd6a486c9e331b10f003
Florentino Carrera <florentinocarrera44@gmail.com>
2023-07-02 22:12:53 +0200

    Desarrollando footer.

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="container">
+      <p className="text-center text-body-secondary">&copy; 2023 Florentino Carrera</p>
+    </footer>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 import Head from 'next/head'
 import Script from 'next/script'
 
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
