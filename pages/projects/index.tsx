ec3e05cfb1c3a4c0211bafd52311843f6c51ae90
Florentino Carrera <florentinocarrera44@gmail.com>
2023-07-28 00:56:33 +0200

    Mejorando descripción de proyectos.

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -24,19 +24,19 @@ const projects = [
   {
     src: "/images/crud.jpg",
     title: "CRUD",
-    text: "CRUD desarrollado con Next.js y MongoDB.",
+    text: "Este CRUD, del acrónimo en inglés Create, Read, Update and Delete, fue desarrollado con Next.js y MongoDB.",
     href: "/projects/crud",
   },
   {
     src: "/images/chat.png",
     title: "Chat",
-    text: "Chat desarrollado con Next.js y OpenAI.",
+    text: "Este es un Chat de inteligencia artificial desarrollado con Next.js y la API de OpenAI.",
     href: "/projects/chat",
   },
   {
     src: "/images/images_generator.png",
     title: "Generador de Imágenes",
-    text: "Generador de Imágenes desarrollado con Next.js y OpenAI.",
+    text: "Este Generador de Imágenes está desarrollado con Next.js y la API de OpenAI.",
     href: "/projects/images-generator",
   },
 ]
